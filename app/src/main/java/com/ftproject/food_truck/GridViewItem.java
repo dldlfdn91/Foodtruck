ef7e36b56b4a63f8d1ae52fd626cc4e12d3a3064
dldlfdn91 <dldlfdn91@naver.com>
2019-11-21 10:41:57 +0900

    Update GridViewItem.java

--- app/src/main/java/com/ftproject/food_truck/GridViewItem.java
+++ app/src/main/java/com/ftproject/food_truck/GridViewItem.java
@@ -2,24 +2,14 @@ package com.ftproject.food_truck;
 
 import android.widget.ImageView;
 
-/**
- * Created by JeongHwan on 2017-05-29.
- */
 public class GridViewItem {
     String name;
     String price;
-   // ImageView imgPhoto;
-   // int imgPhoto;
     String imgURL;
 
-    public GridViewItem(String name, String price,
-                        //ImageView imgPhoto
-                        //int imgPhoto
-                        String imgURL) {
-
+    public GridViewItem(String name, String price, String imgURL) {
         this.name = name;
         this.price = price;
-        //this.imgPhoto = imgPhoto;
         this.imgURL =imgURL;
     }
 
@@ -39,16 +29,9 @@ public class GridViewItem {
         this.price = price;
     }
 
-    /*public /*ImageViewint getImgPhoto() {
-        return imgPhoto;
-    }*/
     public String getImgURL(){
         return imgURL;
     }
-
-    /*public void setImgPhoto(/*ImageView imgPhoto int imgPhoto) {
-        this.imgPhoto = imgPhoto;
-    }*/
 
     public void setImgURL(String imgURL){
         this.imgURL=imgURL;
